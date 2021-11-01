55fbeb8ab48ecd1878f8b02dbb1ee683381d5bd5
Hayolox <akiklii72@gmail.com>
2021-11-01 21:38:56 +0700

    first commit

---
+++ database/migrations/2021_11_01_133548_create_identities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdentitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identities', function (Blueprint $table) {
+            $table->integer('receipt_number')->primary();
+            $table->string('name', 100);
+            $table->string('phone_number', 15);
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('identities');
+    }
+}
---
+++ database/migrations/2021_11_01_135557_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+
+            $table->integer('receipt_number');
+            $table->unsignedBigInteger('package_types_id');
+            $table->string('name_shoe');
+            $table->timestamps();
+
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+        
+            $table->foreign('receipt_number')->references('receipt_number')->on('identities');
+            $table->foreign('package_types_id')->references('id')->on('package_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
